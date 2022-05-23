bf2b73abd036d0229bade26be7dbc478391bad60
Shunsuke Watanabe <ww.shunsuke@gmail.com>
2022-05-23 18:51:12 +0800

    default AccountId

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -14,7 +14,7 @@ use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
 	traits::{AccountIdLookup, BlakeTwo256, Block as BlockT, IdentifyAccount, Verify},
 	transaction_validity::{TransactionSource, TransactionValidity},
-	ApplyExtrinsicResult, MultiSignature, RuntimeDebug,
+	AccountId32, ApplyExtrinsicResult, MultiSignature, RuntimeDebug,
 };
 use sp_std::prelude::*;
 #[cfg(feature = "std")]
@@ -315,17 +315,17 @@ impl pallet_dapps_staking::Config for Runtime {
 	type MaxEraStakeValues = MaxEraStakeValues;
 }
 
+const DEFAULT_ACCOUNT: AccountId = AccountId32::new([0; 32]);
+
 #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, scale_info::TypeInfo)]
 pub enum SmartContract {
 	/// Wasm smart contract instance.
-	/// TODO: Abolish Option, Use AccountId instead.
-	Wasm(Option<AccountId>),
+	Wasm(AccountId),
 }
 
 impl Default for SmartContract {
 	fn default() -> Self {
-		// TODO: Construct concrete AccountId value and return it as default value.
-		SmartContract::Wasm(None)
+		SmartContract::Wasm(DEFAULT_ACCOUNT)
 	}
 }
 
@@ -333,12 +333,10 @@ impl Default for SmartContract {
 impl pallet_dapps_staking::traits::IsContract for SmartContract {
 	fn is_valid(&self) -> bool {
 		match self {
-			SmartContract::Wasm(account) => account.as_ref().map_or(false, |_a| {
-				// temporarilly no AccountId validation.
-				// we want getter function here, so that we can check the existence of contract by
-				// AccountId. https://github.com/paritytech/substrate/blob/7a28c62246406839b746af2201309d0ed9a3f526/frame/contracts/src/lib.rs#L792
-				true
-			}),
+			// temporarilly no AccountId validation.
+			// we want getter function here, so that we can check the existence of contract by AccountId.
+			// https://github.com/paritytech/substrate/blob/7a28c62246406839b746af2201309d0ed9a3f526/frame/contracts/src/lib.rs#L792
+			SmartContract::Wasm(_account) => true,
 		}
 	}
 }
