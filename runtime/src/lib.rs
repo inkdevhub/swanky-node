85b4c3db9ff17828f64342c0e10967183530f056
PierreOssun <pierre.giraud@mailfence.com>
2022-06-02 15:33:54 +0200

    Added chain extension for pallet_asset

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -312,7 +312,7 @@ impl pallet_contracts::Config for Runtime {
 	type DepositPerByte = DepositPerByte;
 	type WeightPrice = pallet_transaction_payment::Pallet<Self>;
 	type WeightInfo = pallet_contracts::weights::SubstrateWeight<Self>;
-	type ChainExtension = ();
+	type ChainExtension = PalletAssetsExtention;
 	type DeletionQueueDepth = DeletionQueueDepth;
 	type DeletionWeightLimit = DeletionWeightLimit;
 	type Schedule = Schedule;
