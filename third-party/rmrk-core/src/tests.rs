4294852fbb9bb88094444e1cbcd106d53c0be433
Mar.io <mario@stake.co.jp>
2022-07-26 15:53:02 +0200

    updated rmrk pallets to lates main c2200e1

--- third-party/rmrk-core/src/tests.rs
+++ third-party/rmrk-core/src/tests.rs
@@ -438,7 +438,6 @@ fn royalty_recipient_default_works() {
 			None,
 		));
 		// Royalty recipient should be BOB
-		// BOOKMARK
 		assert_eq!(RmrkCore::nfts(0, 1).unwrap().royalty.unwrap().recipient, BOB);
 		// Mint another NFT
 		assert_ok!(RMRKCore::mint_nft(
