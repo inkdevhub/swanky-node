7ad09147269c4c74da8c7577cc6b83b56651f177
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2023-03-24 11:27:15 +0900

    added signing chain extension

--- runtime/src/chain_extensions.rs
+++ runtime/src/chain_extensions.rs
@@ -1,5 +1,6 @@
 //!
 use super::Runtime;
+pub use chain_extension_signing::SigningExtension;
 pub use pallet_chain_extension_assets::{weights::WeightInfo, AssetsExtension};
 pub use pallet_chain_extension_dapps_staking::DappsStakingExtension;
 /// Registered WASM contracts chain extensions.
@@ -9,6 +10,10 @@ use pallet_contracts::chain_extension::RegisteredChainExtension;
 
 impl RegisteredChainExtension<Runtime> for DappsStakingExtension<Runtime> {
 	const ID: u16 = 0x0000;
+}
+
+impl RegisteredChainExtension<Runtime> for SigningExtension<Runtime> {
+	const ID: u16 = 0x0001;
 }
 
 /// Based on chain-extension registry https://github.com/paritytech/chainextension-registry
